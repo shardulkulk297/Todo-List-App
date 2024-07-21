f5385350f25dd95ad4661b1b21b3a18003ae064d
Shardulkulk297 <shardulkulk297@gmail.com>
2024-07-22 00:46:17 +0530

    Todo App start

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <H1>TODO APP</H1>
+    </>
+  )
+}
+
+export default App
